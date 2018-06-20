dfccdfb61629e12a615e56cb2c4c538f080e5363
Gift4ick <gifthub.2018@gmail.com>
2018-06-20 14:25:40 +0300

    LJKH

---
+++ Assets/Scripts/Flower.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flower : MonoBehaviour
+{
+    Animator anim;
+    bool IsOpen;
+	// Use this for initialization
+	void Start ()
+    {
+        anim = GetComponent<Animator>();
+	}
+
+    private void OnMouseDown()
+    {
+        IsOpen = !IsOpen;
+        anim.SetBool("IsOpen", IsOpen);
+    }
+}
